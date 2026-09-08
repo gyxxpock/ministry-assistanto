553f316938c77bc5b3aef90022df7f47d5010c4c
gyxxpock <gyxxpock@gmail.com>
2026-09-08 13:36:06 -0600

    Revert "fix: Add app-level shell for persistent navigation"
    This reverts commit e72d4870ae57d147fa657f5debd0cd2da01fe7f7.

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,33 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppShellComponent } from './shared/presentation/components/app-shell/app-shell.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppShellComponent,
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'time-entry/list'
-      },
-      {
-        path: 'time-entry',
-        loadChildren: () =>
-          import('./time-entry/presentation/time-entry.module').then(m => m.TimeEntryModule)
-      },
-      {
-        path: 'goals',
-        loadChildren: () =>
-          import('./goals/presentation/goals.module').then(m => m.GoalsModule)
-      }
-    ]
+    pathMatch: 'full',
+    redirectTo: 'time-entry/list'
+  },
+  {
+    path: 'time-entry',
+    loadChildren: () =>
+      import('./time-entry/presentation/time-entry.module').then(m => m.TimeEntryModule)
+  },
+  {
+    path: 'goals',
+    loadChildren: () =>
+      import('./goals/presentation/goals.module').then(m => m.GoalsModule)
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true }), AppShellComponent],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
